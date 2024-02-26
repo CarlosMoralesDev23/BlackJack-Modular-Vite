83e4c73a6c65c0415965c467bb757719a7f6aef8
CarlosMoralesDev23 <carlosmoralesdev23@gmail.com>
2024-02-25 21:06:13 -0300

    Se crea archivo condicion-puntaje y se deja app funcioanando

--- src/blackjack/usecases/puntosYAsignar.js
+++ src/blackjack/usecases/puntosYAsignar.js
@@ -4,7 +4,6 @@ import { valorCarta } from "./valor-carta";
 export const puntosYAsignar = ( puntos, carta, puntosHTML )=>{
 
     puntos = puntos + valorCarta(carta);
-    console.log(puntosHTML)
     puntosHTML.innerText = puntos;
 
     return puntos
